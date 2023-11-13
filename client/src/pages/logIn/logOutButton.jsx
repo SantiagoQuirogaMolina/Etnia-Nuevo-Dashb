536b6eb878f1347b1a6d6858d03721fd3a5c4145
Jessuss <jesusacosta3321@gmail.com>
2023-11-13 15:18:39 -0300

    Mejorando user-experience login

--- client/src/pages/logIn/logOutButton.jsx
+++ client/src/pages/logIn/logOutButton.jsx
@@ -2,17 +2,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 // import { useAuth0 } from "@auth0/auth0-react";
 
 import {userLogout} from "../../redux/actions";
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   // const { logout } = useAuth0();
 
 
   const handleLogOut = ()=>{
     dispatch(userLogout());
+    navigate("/");
   }
 
   return (
