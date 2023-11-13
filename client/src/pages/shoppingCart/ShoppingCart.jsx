000c3dbe775a1379e067fdd43ee1a9b527b7029f
EDMR <emermontes15@gmail.com>
2023-11-13 01:24:45 -0500

    [UPDATE]Estilos carritos

--- client/src/pages/shoppingCart/ShoppingCart.jsx
+++ client/src/pages/shoppingCart/ShoppingCart.jsx
@@ -37,6 +37,15 @@ function ShoppingCart() {
     dispatch(removeFromCart(productId));
 
   };
+  // const numberMore = (event, productId)=>{
+  //   const newValue = parseInt(event.target.value, 10) +1
+
+  //   setQuantities(prevQuantities => ({
+  //     ...prevQuantities,
+  //     [productId]: newValue
+  //   }));
+
+  // }
 
   const handleQuantityChange = (productId, event, cantidad) => {
       
@@ -98,25 +107,35 @@ function ShoppingCart() {
             <div key={Object.values(carts.size)} className={styles['cart-item']}>
               <img src={carts.img} alt={carts.name} />
               <div className={styles['product-details']}>
-                <h2>{carts.name}</h2>
-                <p className={styles.price}>Color: {carts.color}</p>
-                <p className={styles.price}>Precio unitario: ${carts.price?.toLocaleString()}</p>
-                <p className={styles.price}>Talla: {Object.keys(carts.size)}</p>
+                <h4>{carts.name}</h4>
+                <div className={styles.detalles}>
+                <p className={styles.price}> <b>Color:</b>  {carts.color}</p>
+                <p className={styles.price}> <b>Talla:</b> {Object.keys(carts.size)}</p>
                 <div className={styles['cart-controls']}>
                   <div className={styles.quantity} >
-                   <p>Cantidad: </p>
-                   <input
-                     type='number'
-                     name={carts.price}
-                     value={quantities[JSON.stringify(carts.size)] }
-                     onChange={(e) => handleQuantityChange(JSON.stringify(carts.size), e, Object.values(carts.size))}
-                   />
-                   <p>{error[JSON.stringify(carts.size)] === true ? 'cantidad invalida' : `cantidad maxima: ${Object.values(carts.size)}` }</p>
+                    <b>Cantidad: </b>
+                    <div className={styles.numberControl}>
+                      <buttom className={styles.numberLeft}/>
+                        <input
+                         type='number'
+                         className={styles.numberQuantity}
+                         name={carts.price}
+                         value={quantities[JSON.stringify(carts.size)] }
+                         onChange={(e) => handleQuantityChange(JSON.stringify(carts.size), e, Object.values(carts.size))}
+                         />
+                      <buttom className={styles.numberRight}/>
+                     </div>
+                   <p className={error[JSON.stringify(carts.size)] === true ? styles.avisoRed : styles.aviso}> {error[JSON.stringify(carts.size)] === true ? 'Cantidad inválida' : `Cantidad máxima: ${Object.values(carts.size)}` }</p>
+                     </div>
                   </div>
+                  <p className={styles.price}> <b> Precio unitario: </b>  ${carts.price?.toLocaleString()}</p>
+                  <button onClick={()=>handleRemoveFromCart(carts.size)} className={styles.btn}>
+                   <svg viewBox="0 0 15 17.5" height="17.5" width="15" xmlns="http://www.w3.org/2000/svg" className={styles.icon}>
+                   <path transform="translate(-2.5 -1.25)" d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z" id="Fill"/>
+                   </svg>
+                 </button>
                 </div>
-                <div className={styles.closeButton}>
-                  <button onClick={()=>handleRemoveFromCart(carts.size)}>X</button>
-                </div>
+                
               </div>
             </div>
           )
